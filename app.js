e52bffdeeb64277ef6f90a918fe41541dd65ca1b
ratigugutishvili <ratigugutishvili2004@mail.ru>
2024-01-06 19:48:58 +0400

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ app.get('/sales', async (req,res)=>{
     res.json({sales:sales})
 })
 
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://localhost:3000', 'https://qavtasi.ge'];
+    const origin = req.headers.origin;
+  
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+  
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.post('/newsale',(req,res)=>{
     const { name ,city, mobile ,email,value,adress } = req.body
     
